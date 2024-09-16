5e5d6a4e44ec3ee6962e78fad74a42f7ebb94de4
Ram Gurung <gurungram2263@gmail.com>
2024-09-16 13:25:03 +0545

    tailwind sample setup in react app

---
+++ Week 2 React/tailwind-setup/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className=' text-red-500'>
+    Hello
+
+    <h2 className=' text-4xl font-bold '>
+    This is big text
+    </h2>
+   </div>
+  )
+}
+
+export default App
