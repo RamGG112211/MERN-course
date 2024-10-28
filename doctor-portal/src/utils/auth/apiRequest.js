afa66b57723366d5e5fe2d5df2ae85b48a73ccde
Ram Gurung <gurungram2263@gmail.com>
2024-10-28 16:56:56 +0545

    api url for production changed

--- doctor-portal/src/utils/auth/apiRequest.js
+++ doctor-portal/src/utils/auth/apiRequest.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001"; // Replace with your API base URL
+const API_BASE_URL = "https://mern-course-xg0p.onrender.com";
+// const API_BASE_URL = "http://localhost:3001";
+
+// Replace with your API base URL
 
 export const apiRequest = async ({
   method,
